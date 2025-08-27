c70257cc0dd00f60ca9f76e44b2cf12565027ce1
Scott Petrovic <scottpetrovic@gmail.com>
2025-08-27 15:37:30 -0500

    Simplified managing origin markers a bit

--- src/lib/processes/load-skeleton/OriginMarkerManager.ts
+++ src/lib/processes/load-skeleton/OriginMarkerManager.ts
@@ -6,42 +6,35 @@ export function add_origin_markers (root: Scene): void {
   remove_origin_markers(root)
 
   // Create a group to hold all markers
-  let markerGroup = root.getObjectByName(ORIGIN_MARKER_GROUP_NAME) as Group
-  if (!markerGroup) {
-    markerGroup = new Group()
-    markerGroup.name = ORIGIN_MARKER_GROUP_NAME
-    root.add(markerGroup)
+  let marker_group = root.getObjectByName(ORIGIN_MARKER_GROUP_NAME) as Group
+  if (!marker_group) {
+    marker_group = new Group()
+    marker_group.name = ORIGIN_MARKER_GROUP_NAME
+    root.add(marker_group)
   }
 
   // with the root scene, there is a group called "Imported Model" that contains
   // the meshes we want to put markers on
-  const imported_meshes: Mesh[] = root.children.find(x => x.name === 'Imported Model')
+  const imported_meshes: Scene = root.children.find(x => x.name === 'Imported Model') as Scene
 
-  const meshes: Mesh[] = []
-  imported_meshes.children.forEach(element => {
-    if (element.type === 'Mesh') {
-      meshes.push(element)
-    }
-  })
-
-  for (const mesh of meshes) {
+  for (const mesh of imported_meshes.children) {
     // Get mesh world position
-    const worldPos = new Vector3()
-    mesh.getWorldPosition(worldPos)
+    const world_pos = new Vector3()
+    mesh.getWorldPosition(world_pos)
 
     const marker = new Mesh(
       new SphereGeometry(0.02, 8, 8),
       new MeshBasicMaterial({ color: 0xff4c00 }) // orange
     )
     marker.name = 'origin_marker_' + mesh.name
-    marker.position.copy(worldPos)
-    markerGroup.add(marker)
+    marker.position.copy(world_pos)
+    marker_group.add(marker)
   }
 }
 
 export function remove_origin_markers(root: Object3D): void {
-  const markerGroup = root.getObjectByName(ORIGIN_MARKER_GROUP_NAME)
-  if (markerGroup && markerGroup.parent) {
-    markerGroup.parent.remove(markerGroup)
+  const marker_group = root.getObjectByName(ORIGIN_MARKER_GROUP_NAME)
+  if (marker_group !== undefined && marker_group.parent) {
+    marker_group.parent.remove(marker_group)
   }
 }

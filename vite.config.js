7d1e82b033e749ce541fadd3a32c3f41a37dfb97
Scott Petrovic <scottpetrovic@gmail.com>
2025-10-16 17:22:27 -0500

    Try to use a config change to make both pages work

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,33 @@
 const isCodeSandbox = 'SANDBOX_URL' in process.env || 'CODESANDBOX_HOST' in process.env
 
+import { resolve } from 'path'
+
 // allows us to use external shaders files to be imported into our materials
 import glsl from 'vite-plugin-glsl'
 
 export default {
-    root: 'src/',
-    publicDir: '../static/',
-    base: './',
-    server:
+  root: 'src/',
+  publicDir: '../static/',
+  base: './',
+  server:
     {
-        host: true,
-        open: !isCodeSandbox // Open if it's not a CodeSandbox
+      host: true,
+      open: !isCodeSandbox // Open if it's not a CodeSandbox
     },
-    build:
+  build:
     {
-        outDir: '../dist',
-        emptyOutDir: true,
-        sourcemap: true
+      outDir: '../dist',
+      emptyOutDir: true,
+      sourcemap: true,
+      rollupOptions: {
+        input: {
+          main: resolve(__dirname, 'src/index.html'),
+          create: resolve(__dirname, 'src/create.html')
+        }
+      }
     },
-    plugins:
+  plugins:
     [
-        glsl()
+      glsl()
     ]
 }
